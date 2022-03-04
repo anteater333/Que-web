4dd4a80db734b9a71e9e2a0766c7d562f999f702
Ji-hoon Lee <anteater1056@gmail.com>
2022-03-04 16:12:29 +0900

    [#6] test - test code 형태 잡아가는 중 먼저 테스트 할 상황에 대해 묘사를 해두는 것이 유효할 것으로 보입니다. 테스트 코드 작성의 감을 잡는 데에 도움을 줄 것 같습니다.

--- app/components/cards/VideoCard.test.js
+++ app/components/cards/VideoCard.test.js
@@ -1,14 +1,53 @@
 import React from "react";
-import renderer from "react-test-renderer";
-import { waitFor } from "@testing-library/react-native";
+import { cleanup, render } from "@testing-library/react-native";
+import { toHaveStyle } from "@testing-library/jest-native";
 
 import VideoCard from "./VideoCard";
+import { Image, Text } from "react-native";
 
-describe("<VideoCard />", () => {
-  it("자식 둘을 가진다.", async () => {
-    const tree = renderer.create(<VideoCard />);
-    const treeJSON = tree.toJSON();
+let component = render(<VideoCard />);
+let componentJSON = component.toJSON();
 
-    expect(treeJSON.children.length).toBe(2);
+beforeEach(() => {
+  component = render(<VideoCard />);
+});
+
+afterEach(cleanup);
+
+describe("VideoCard", () => {
+  expect.extend({ toHaveStyle });
+
+  it("두 View 영역으로 나뉜다.", () => {
+    expect(componentJSON.children.length).toBe(2);
+    expect(componentJSON.children[0].type).toBe("View");
+    expect(componentJSON.children[1].type).toBe("View");
+  });
+
+  it("카드를 누를 시 VideoScene으로 이동한다.", () => {});
+
+  describe("첫 번째 View 영역", () => {
+    it("이미지, 시간, 추가 옵션 버튼으로 이루어진다.", () => {
+      const view = component.getByTestId("cardThumbnailView");
+      const image = component.getByTestId("cardThumbnailImage");
+      const time = component.getByTestId("cardThumbnailTime");
+      const button = component.getByTestId("cardThumbnailButton");
+
+      expect(view.children.length).toBe(3);
+      expect(image.props.source.uri).not.toBe("");
+    });
+
+    const cardThumbnailStyle = {};
+    it("cardThumbnailStyle을 따른다.", () => {});
+
+    it("버튼을 누를 시 추가 메뉴 모달을 출력한다.", () => {});
+  });
+
+  describe("두 번재 view 영역", () => {
+    it("프로필 사진, 제목, 사용자 이름, 시청 수, 좋아요, 평가점수로 이루어진다.", () => {});
+
+    const cardInfoStyle = {};
+    it("cardInfoStyle을 따른다.", () => {});
+
+    it("프로필 사진을 누르면 StudioScene으로 이동한다.", () => {});
   });
 });
