7bcb3833d70a841937125838bc16d63e4c951fc0
Ji-hoon Lee <anteater1056@gmail.com>
2022-03-26 18:35:52 +0900

    [#13] test - resource api unit test code

--- app/api/firebase/firestore/firestore.ts
+++ app/api/firebase/firestore/firestore.ts
@@ -3,5 +3,5 @@ import VideoType from "../../../types/Video";
 export async function getVideoCardDataFromFirestore(
   page: number
 ): Promise<VideoType[]> {
-  throw new Error();
+  throw new Error("요 브로, 디스 메소드 낫 임플리멘티드 옛.");
 }
